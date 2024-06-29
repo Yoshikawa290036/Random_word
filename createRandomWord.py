435aa8964bcb4a6ecab9fcf15b3d512a583eb8ef
Yoshikawa290036 <290036hirokix@gmail.com>
2024-06-29 16:14:08 +0900

    [Add] createRandomWord.py. \n[update] test.yml branch

---
+++ createRandomWord.py
@@ -0,0 +1,22 @@
+import random
+import datetime
+
+LOG_FNAME = ".log"
+t_delta = datetime.timedelta(hours=9)
+JST = datetime.timezone(t_delta, 'JST')
+
+
+def main():
+    key_word = "hello"
+    now = datetime.datetime.now(JST)
+    time_str = now.strftime('%Y %m %d %H %M %S')
+    shuffuled_key_word = ''.join(random.sample(key_word, len(key_word)))
+
+    outline = f"{time_str}\t{shuffuled_key_word}\n"
+    with open(LOG_FNAME, 'a') as f:
+        f.write(outline)
+    print(outline, end="")
+
+
+if __name__ == "__main__":
+    main()
